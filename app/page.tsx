362d9a5a3bc733b8412bb8c3f5ba326af8887da4
Ucheze Victor <uchezevictor6@gmail.com>
2025-04-05 13:35:11 +0100

    Added border color on focus

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <form action="" className="w-full">
             <input
               type="number"
-              className="py-2 px-4 mb-4 w-full outline-none border-solid border-gray-300 border-2 rounded-xl"
+              className="py-2 px-4 mb-4 w-full outline-none border-solid border-gray-300 border-2 focus:border-blue-500 rounded-xl"
               placeholder="Student Id"
             />
 
@@ -37,9 +37,9 @@ export default function Home() {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                     />
                   </svg>
